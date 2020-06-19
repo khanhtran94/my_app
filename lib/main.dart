530eeffdd3c1db0aa8ea78eb6a7cd3c840de4148
khanhTran <khanhtrantung190894@gmail.com>
2020-06-20 00:14:35 +0700

    add lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        
-      )
-      );
-  }
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
 
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('WordPair Generator'),
+          ),
+          body: Center(
+            child: Text(wordPair.asPascalCase),
+          ),
+        ));
+  }
 }
