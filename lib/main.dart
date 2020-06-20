21464d864c1abd0e7cf37647eb6f6894e8ddcbe8
khanhTran <khanhtrantung190894@gmail.com>
2020-06-20 11:14:44 +0700

    youtobe finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:my_app/random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,13 +11,6 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('WordPair Generator'),
-          ),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-          ),
-        ));
+        home: RandomWords());
   }
 }
