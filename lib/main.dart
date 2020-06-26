8d851bda61098022bd5f7d309e53fa112c2b2276
khanhTran <khanhtrantung190894@gmail.com>
2020-06-26 23:40:52 +0700

    tach file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'facyButton.dart';
 
 void main() {
   runApp(MyApp());
@@ -138,41 +137,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class FancyButton extends StatefulWidget {
-  final VoidCallback onPressed;
-  final Widget child;
-  const FancyButton({Key key, this.onPressed, this.child}) : super(key: key);
-  @override
-  _FancyButtonState createState() => _FancyButtonState();
-}
-
-class _FancyButtonState extends State<FancyButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        color: _getColors(),
-        child: widget.child,
-        onPressed: widget.onPressed,
-      ),
-    );
-  }
-
-  Color _getColors() {
-    return _buttonColors.putIfAbsent(this, () => colors[next(0, 5)]);
-  }
-}
-
-Map<_FancyButtonState, Color> _buttonColors = {};
-final _random = Random();
-int next(int min, int max) => min + _random.nextInt(max - min);
-List<Color> colors = [
-  Colors.blue,
-  Colors.green,
-  Colors.orange,
-  Colors.purple,
-  Colors.red,
-  Colors.amber,
-  Colors.lightBlue
-];
